50434aa3b2735b2bff155cc667b9ee3bbc83669e
Liang Wang <wang.leon@gmail.com>
2018-08-24 16:51:48 +0200

    add new functions to Xinglong216HRS pipelines

--- edrs/pipelines/xinglong216hrs.py
+++ edrs/pipelines/xinglong216hrs.py
@@ -5,14 +5,296 @@ import logging
 logger = logging.getLogger(__name__)
 
 import numpy as np
+from scipy.signal import savgol_filter
+from scipy.ndimage.filters import gaussian_filter
+from scipy.interpolate import InterpolatedUnivariateSpline
 import astropy.io.fits as fits
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tck
 
-from ..utils             import obslog
-from ..echelle.imageproc import array_to_table, fix_pixels
+from ..utils import obslog
+from ..utils.config import read_config
+from ..echelle.imageproc import (combine_images, array_to_table,
+                                 table_to_array, fix_pixels)
 from .reduction          import Reduction
 
+def fix_cr(data):
+    m = data.mean(dtype=np.float64)
+    s = data.std(dtype=np.float64)
+    mask = data > m + 3.*s
+    if mask.sum()>0:
+        x = np.arange(data.size)
+        f = InterpolatedUnivariateSpline(x[~mask], data[~mask], k=3)
+        return f(x)
+    else:
+        return data
+
+def get_badpixel_mask(shape, bins):
+    mask = np.zeros(shape, dtype=np.bool)
+    if bins == (1, 1) and shape == (4136, 4096):
+        h, w = shape
+
+        mask[349:352, 627:630] = True
+        mask[349:h//2, 628]    = True
+
+        mask[1604:h//2, 2452] = True
+
+        mask[280:284,3701]   = True
+        mask[274:h//2, 3702] = True
+        mask[272:h//2, 3703] = True
+        mask[274:282, 3704]  = True
+
+        mask[1720:1722, 3532:3535] = True
+        mask[1720, 3535]           = True
+        mask[1722, 3532]           = True
+        mask[1720:h//2,3533]       = True
+
+        mask[347:349, 4082:4084] = True
+        mask[347:h//2,4083]      = True
+
+        mask[h//2:2631, 1909] = True
+    else:
+        print('No bad pixel information for this CCD size.')
+        raise ValueError
+    return mask
+
+def get_mask(data, head):
+    '''Get the mask of input image.
+
+    Args:
+        data (:class:`numpy.array`):
+        head (:class:`astropy.io.fits.Header`):
+
+    Returns:
+        :class:`numpy.array`: Image mask.
+
+    The shape of output mask is determined by the keywords in the input FITS
+    header according to the relation below::
+     
+        *N*:sub:`columns` = head['NAXIS1'] - head['COVER']
+
+        *N*:sub:`rows` = head['NAXIS2'] - head['ROVER']
+
+    '''
+
+    saturation_adu = 65535
+
+    # determine shape of output image (also the shape of science region)
+    y1 = head['CRVAL2']
+    y2 = y1 + head['NAXIS2'] - head['ROVER']
+    x1 = head['CRVAL1']
+    x2 = x1 + head['NAXIS1'] - head['COVER']
+    newshape = (y2-y1, x2-x1)
+
+    # find the saturation mask
+    mask_sat = (data[y1:y2, x1:x2]>=saturation_adu)
+    # get bad pixel mask
+    bins = (head['RBIN'], head['CBIN'])
+    mask_bad = get_badpixel_mask(newshape, bins=bins)
+
+    mask = np.int16(mask_sat)*4 + np.int16(mask_bad)*2
+
+    return mask
+
+
+def correct_overscan(data, head, mask=None):
+    '''Correct overscan.
+    
+    Args:
+        data (:class:`numpy.array`):
+        head (:class:`astropy.io.fits.Header`):
+        mask (:class:`numpy.array`):
+    
+    Returns:
+        A tuple containing:
+            * data (:class:`numpy.array`): The output image with overscan
+                corrected.
+            * head (:class:`astropy.io.fits.Header`): The updated FITS header.
+    '''
+
+    h, w = data.shape
+    x1, x2 = w-head['COVER'], w
+
+    # find the overscan level along the y-axis
+    ovr_lst1 = data[0:h//2,x1+2:x2].mean(dtype=np.float64, axis=1)
+    ovr_lst2 = data[h//2:h,x1+2:x2].mean(dtype=np.float64, axis=1)
+
+    ovr_lst1_fix = fix_cr(ovr_lst1)
+    ovr_lst2_fix = fix_cr(ovr_lst2)
+
+    # apply the sav-gol fitler to the mean of overscan
+    ovrsmooth1 = savgol_filter(ovr_lst1_fix, window_length=301, polyorder=3)
+    ovrsmooth2 = savgol_filter(ovr_lst2_fix, window_length=301, polyorder=3)
+
+    # determine shape of output image (also the shape of science region)
+    y1 = head['CRVAL2']
+    y2 = y1 + head['NAXIS2'] - head['ROVER']
+    ymid = (y1 + y2)//2
+    x1 = head['CRVAL1']
+    x2 = x1 + head['NAXIS1'] - head['COVER']
+    newshape = (y2-y1, x2-x1)
+
+    # subtract overscan
+    new_data = np.zeros(newshape, dtype=np.float64)
+    ovrdata1 = np.repeat([ovrsmooth1],x2-x1,axis=0).T
+    ovrdata2 = np.repeat([ovrsmooth2],x2-x1,axis=0).T
+    new_data[y1:ymid, x1:x2] = data[y1:ymid,x1:x2] - ovrdata1
+    new_data[ymid:y2, x1:x2] = data[ymid:y2,x1:x2] - ovrdata2
+
+    # fix bad pixels
+    bad_mask = (mask&2 > 0)
+    new_data = fix_pixels(new_data, bad_mask, 'x', 'linear')
+
+    # update fits header
+    head['HIERARCH EDRS OVERSCAN']        = True
+    head['HIERARCH EDRS OVERSCAN METHOD'] = 'smooth'
+
+    return newdata, head
+
+def reduce():
+    '''Reduce the Xinglong 216/HRS spectra.
+    '''
+
+    obslogfile = obslog.find_log(os.curdir)
+    log = obslog.read_log(obslogfile)
+
+    config = read_config('Xinglong216HRS')
+
+    rawdata = config['data']['rawdata']
+
+    # parse bias
+    if os.path.exists('bias.fits'):
+        bias = fits.getdata('bias.fits')
+    else:
+        bias_lst = []
+        for item in log:
+            if item.objectname[0].strip().lower()=='bias':
+                filename = os.path.join(rawdata, '%s.fits'%item.fileid)
+                data, head = fits.getdata(filename, header=True)
+                mask = get_mask(data, head)
+                data, head = correct_overscan(data, head, mask)
+                bias_lst.append(data)
+
+        if len(bias_lst)>0:
+            # there is bias frames
+            if not config.has_section('bias'):
+                print('Warning: Configuration file has no bias section')
+                exit()
+
+            bias = combine_images(bias_lst,
+                                    mode       = 'mean',
+                                    upper_clip = config['bias']['cosmic_clip'],
+                                    maxiter    = 5,
+                                    )
+
+            # create new FITS Header for bias
+            head = fits.Header()
+            head['HIERARCH EDRS BIAS NFILE'] = len(bias_id_lst)
+
+            if config.has_option('bias', 'smooth_method'):
+                # perform smoothing for bias
+
+                h, w = bias.shape
+                smooth_method = config['bias']['smooth_method']
+                if smooth_method in ['gauss','gaussian']:
+                    # perform 2D gaussian smoothing
+                    smooth_sigma = int(config['bias']['smooth_sigma'])
+                    smooth_mode  = config['bias']['smooth_mode']
+                    bias_smooth = np.zeros((h, w), dtype=np.float64)
+                    bias_smooth[0:h/2, :] = gaussian_filter(bias[0:h/2, :],
+                                                            smooth_sigma,
+                                                            mode=smooth_mode)
+                    bias_smooth[h/2:h, :] = gaussian_filter(bias[h/2:h, :],
+                                                            smooth_sigma,
+                                                            mode=smooth_mode)
+                    # write information to FITS header
+                    head['HIERARCH EDRS BIAS SMOOTH']        = True
+                    head['HIERARCH EDRS BIAS SMOOTH METHOD'] = 'GAUSSIAN'
+                    head['HIERARCH EDRS BIAS SMOOTH SIGMA']  = smooth_sigma
+                    head['HIERARCH EDRS BIAS SMOOTH MODE']   = smooth_mode
+                else:
+                    print('Unknown smooth method: ', smooth_method)
+                    pass
+
+                bias = bias_smooth
+
+            fits.writeto('bias.fits', bias, header=head, overwrite=True)
+        else:
+            # no bias in this dataset
+            bias = 0
+
+    # find flat groups
+    # initialize flat_groups for single fiber
+    flat_groups = {}
+    for item in log:
+        name = item.objectname[0]
+        g = name.split()
+        if len(g)>0 and g[0].lower().strip() == 'flat':
+            # the object name of the channel matches "flat ???"
+
+            # find a proper name for this flat
+            if name.lower().strip()=='flat':
+                # no special names given, use "flat_A_15"
+                flatname = 'flat_%g'%(item.exptime)
+            else:
+                # flatname is given. replace space with "_"
+                # remove "flat" before the objectname. e.g.,
+                # "Flat Red" becomes "Red" 
+                char = name[4:].strip()
+                flatname = 'flat_%s'%(char.replace(' ','_'))
+
+            # add flatname to flat_groups
+            if flatname not in flat_groups:
+                flat_groups[flatname] = []
+            flat_groups[flatname].append(item.fileid)
+
+    # trace the orders
+    if os.path.exists('trace.trc'):
+        aperset = load_aperture_set('trace.trc')
+    else:
+        # use flats to trace the orders
+
+        # first combine the flats
+        for flatname, fileids in flat_groups.items():
+            flat_filename = '%s.fits'%flatname
+            mask_filename = '%s_msk.fits'%flatname
+            if os.path.exists(flat_filename) and os.path.exists(mask_filename):
+                flat_data  = fits.getdata(flat_filename)
+                mask_table = fits.getdata(mask_filename)
+                mask_array = table_to_array(mask_table, flat_data.shape)
+            else:
+                data_lst = []
+                for ifile, fileid in enumerate(fileids):
+                    filename = os.path.join(rawdata, '%s.fits'%fileid)
+                    data, head = fits.getdata(filename, header=True)
+
+                    # determine the science region
+                    y1 = head['CRVAL2']
+                    y2 = y1 + head['NAXIS2'] - head['ROVER']
+                    x1 = head['CRVAL1']
+                    x2 = x1 + head['NAXIS1'] - head['COVER']
+
+                    mask = (data[y1:y2, x1:x2]==65535)
+                    if ifile==0:
+                        allmask = np.zeros_like(mask, dtype=np.int16)
+                    allmask += mask
+                    data, head = correct_overscan(data, head)
+                    data = data - bias
+                    data_lst.append(data)
+                nflat = len(data_lst)
+                print('combine %d images for %s'%(nflat, flatname))
+                flat_data = combine_images(data_lst, mode='mean',
+                                            upper_clip=10, maxiter=5)
+                # write combine result to fits
+                fits.writeto(flat_filename, flat_data, overwrite=True)
+                # find saturation mask
+                sat_mask = allmask > nflat/2.
+                mask_array = np.int16(sat_mask)*4
+                mask_table = array_to_table(mask_array)
+                # write mask to fits
+                fits.writeto(mask_filename, mask_table, overwrite=True)
+
+    
 class Xinglong216HRS(Reduction):
 
     def __init__(self):
@@ -38,8 +320,6 @@ class Xinglong216HRS(Reduction):
 
         '''
 
-        from scipy.signal import savgol_filter
-        from scipy.interpolate import InterpolatedUnivariateSpline
 
         def fix_cr(a):
             m = a.mean(dtype=np.float64)
