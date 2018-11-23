9e6d5ff26bc18a02ff6c3ab3f7ffec0f0e4e455f
Liang Wang <wang.leon@gmail.com>
2018-11-23 23:49:34 +0100

    add a new background correction function

--- edrs/echelle/background.py
+++ edrs/echelle/background.py
@@ -1,4 +1,5 @@
 import os
+import math
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,10 +13,12 @@ import matplotlib.colors as colors
 import matplotlib.cm     as cmap
 import matplotlib.ticker as tck
 
+from ..utils.onedarray    import get_local_minima
+from ..utils.regression   import get_clip_mean
 from ..utils.regression2d import polyfit2d, polyval2d
 from .imageproc           import table_to_array, array_to_table
 
-def find_background(data, mask, channels, apertureset_lst,
+def find_background2(data, mask, channels, apertureset_lst,
         method='poly', scale='linear', scan_step=200,
         xorder=2, yorder=2, maxiter=5, upper_clip=3, lower_clip=3,
         extend=True, display=True, fig_file=None, reg_file=None):
@@ -28,29 +31,29 @@ def find_background(data, mask, channels, apertureset_lst,
         channels (list): List of channels as strings.
         apertureset_lst (dict): Dict of :class:`~edrs.echelle.trace.ApertureSet`
             at different channels.
-        method (string): Method of finding background light.
-        scale (string): Scale of the 2-D polynomial fitting. If 'log', fit the
+        method (str): Method of finding background light.
+        scale (str): Scale of the 2-D polynomial fitting. If 'log', fit the
             polynomial in the logrithm scale.
-        scan_step (integer): Steps of scan in pixels.
-        xorder (integer): Order of 2D polynomial along the main dispersion
-            direction (only applicable when **method** = "poly").
-        yorder (integer): Order of 2D polynomial along the cross-dispersion
-            direction (only applicable when **method** = "poly").
-        maxiter (integer): Maximum number of iteration of 2D polynomial fitting
-            (only applicable when **method** = "poly").
-        upper_clip (float): Upper sigma clipping threshold (only applicable when
+        scan_step (int): Steps of scan in pixels.
+        xorder (int): Order of 2D polynomial along the main dispersion
+            direction (only applicable if **method** = "poly").
+        yorder (int): Order of 2D polynomial along the cross-dispersion
+            direction (only applicable if **method** = "poly").
+        maxiter (int): Maximum number of iteration of 2D polynomial fitting
+            (only applicable if **method** = "poly").
+        upper_clip (float): Upper sigma clipping threshold (only applicable if
             **method** = "poly").
-        lower_clip (float): Lower sigma clipping threshold (only applicable when
+        lower_clip (float): Lower sigma clipping threshold (only applicable if
             **method** = "poly").
         extend (bool): Extend the grid to the whole CCD image if *True*.
         display (bool): Display figures on the screen if *True*.
-        fig_file (string): Name of the output figure. No image file generated if
+        fig_file (str): Name of the output figure. No image file generated if
             *None*.
         reg_file (string): Name of the output DS9 region file. No file generated
             if *None*.
 
     Returns:
-        :class:`numpy.ndarray`: Array of background light.
+        :class:`numpy.ndarray`: Image of background light.
     '''
     
     plot = (display or fig_file is not None)
@@ -94,9 +97,10 @@ def find_background(data, mask, channels, apertureset_lst,
                         mid_newy = (prev_newy + this_newy)//2
                         i1 = min(h-1, max(0, int(prev_newy)))
                         i2 = min(h-1, max(0, int(this_newy)))
-                        if len(inter_aper)==0 or \
-                            abs(mid_newy - inter_aper[-1])>scan_step*0.7:
-                            if i2-i1>0:
+                        #if len(inter_aper)==0 or \
+                        #    abs(mid_newy - inter_aper[-1])>scan_step*0.7:
+                        #    if i2-i1>0:
+                        if i2-i1>0:
                                 mid_newy = i1 + xsection[i1:i2].argmin()
                                 inter_aper.append(mid_newy)
                     prev_newy = this_newy
@@ -110,7 +114,21 @@ def find_background(data, mask, channels, apertureset_lst,
         # cover the whole CCD area
         n_nodes_extend = 0
         if extend:
+            if x==2304:
+                _fig = plt.figure(dpi=150)
+                _ax = _fig.gca()
+                for _x in inter_aper:
+                    _ax.axvline(x=_x,color='g', ls='--',lw=0.5, alpha=0.6)
+                _ax.plot(data[:, x],'b-',lw=0.5)
+                _fig2 = plt.figure(dpi=150)
+                _ax2 = _fig2.gca()
+            print(inter_aper)
+
             coeff = np.polyfit(np.arange(inter_aper.size), inter_aper, deg=3)
+            if x== 2304:
+                _ax2.plot(np.arange(inter_aper.size), inter_aper,'go', alpha=0.6)
+                _newx = np.arange(0, inter_aper.size, 0.1)
+                _ax2.plot(_newx, np.polyval(coeff, _newx),'g-')
             # find the points after the end of inter_aper
             ii = inter_aper.size-1
             new_y = inter_aper[-1]
@@ -127,6 +145,12 @@ def find_background(data, mask, channels, apertureset_lst,
                 new_y = int(np.polyval(coeff,ii))
                 inter_aper = np.insert(inter_aper,0,new_y)
                 n_nodes_extend += 1
+            if x==2304:
+                #for _x in np.polyval(coeff, np.arange(0,25)):
+                #    _ax.axvline(x=_x, color='r',ls='--',lw=0.5)
+                #_newx = np.arange(0, 25)
+                #_ax2.plot(_newx, np.polyval(coeff, _newx), 'ro', alpha=0.6)
+                plt.show()
 
         # remove those points with y<0 or y>h-1
         m1 = inter_aper > 0
@@ -396,11 +420,11 @@ def fit_background(shape, xnodes, ynodes, znodes, xorder=2, yorder=2,
         xnodes (:class:`numpy.ndarray`): List of X coordinates of the nodes.
         ynodes (:class:`numpy.ndarray`): List of Y coordinates of the nodes.
         znodes (:class:`numpy.ndarray`): List of pixel values of the nodes.
-        xorder (integer): Order of 2D polynomial along the main dispersion
+        xorder (int): Order of 2D polynomial along the main dispersion
             direction.
-        yorder (integer): Order of 2D polynomial along the cross-dispersion
+        yorder (int): Order of 2D polynomial along the cross-dispersion
             direction.
-        maxiter (integer): Maximum number of iteration of 2D polynomial fitting.
+        maxiter (int): Maximum number of iteration of 2D polynomial fitting.
         upper_clip (float): Upper sigma clipping threshold.
         lower_clip (float): Lower sigma clipping threshold.
 
@@ -493,3 +517,208 @@ def interpolate_background(shape, xnodes, ynodes, znodes):
             background_data[i2+1:, j] = array[i2]
 
     return background_data, mask
+
+def find_background(data, mask, apertureset_lst, ncols, distance,
+        yorder=7, ymaxiter=5, yupper_clip=3, ylower_clip=3,
+        fig_stray=None, fig_section=None):
+
+    '''Subtract the background for an input FITS image.
+
+    Args:
+        data (:class:`numpy.ndarray`): Input data image.
+        mask (:class:`numpy.ndarray`): Mask of input data image.
+        apertureset_lst (dict): Dict of :class:`~edrs.echelle.trace.ApertureSet`
+            at different channels.
+        yorder (int): Order of polynomial along the cross-dispersion
+            direction.
+        fig_stray (str): Name of the figure showing stray light. No file
+            generated if *None*.
+        fig_section (str): Name of the figure showing cross-sections. No file
+            generated if *None*.
+
+    Returns:
+        :class:`numpy.ndarray`: Image of background light. It has the same shape
+            and datatype as the arg **data**.
+    '''
+
+    h, w = data.shape
+    cols = np.int32(np.round(np.linspace(1, w-2, ncols)))
+
+    # prepare for cross-section figure
+    plot_section = (fig_section is not None)
+    if plot_section:
+        plot_cols = [cols[np.abs(cols - h*t).argmin()]
+                        for t in np.linspace(0, 1, 5)]
+        fig1 = plt.figure(figsize=(18,12), dpi=150)
+
+    ally = np.arange(h)
+
+    # prepare interpolation grid
+    grid = []
+
+    for x in cols:
+        xsection = np.median(data[:,x-1:x+2], axis=1)
+        intermask = np.ones(h, dtype=np.bool)
+        allimin, _ = get_local_minima(xsection)
+        allmin = np.array([v in allimin for v in np.arange(h)])
+
+        if plot_section and x in plot_cols:
+            i = plot_cols.index(x)
+            ax1 = fig1.add_axes([0.05, (4-i)*0.19+0.05, 0.9, 0.18])
+            ax2 = ax1.twinx()
+            ax1.plot(xsection, ls='-' ,color='C0', lw=0.5, alpha=0.2)
+
+        for ichannel, (channel, apertureset) in enumerate(sorted(apertureset_lst.items())):
+            # predict post- and pre-aperutres
+            y_lst, aper_lst = [], []
+            apercen_lst = []
+            for aper, aperloc in apertureset.items():
+                y = aperloc.position(x)
+                apercen_lst.append(y)
+                y_lst.append(y)
+                aper_lst.append(aper)
+            y_lst = np.array(y_lst)
+            aper_lst = np.array(aper_lst)
+            apercen_lst = np.array(apercen_lst)
+            coeff = np.polyfit(aper_lst, y_lst, deg=3)
+     
+            # find post apertures
+            aper = aper_lst[-1]
+            post_aper_lst, post_apercen_lst = [], []
+            while(True):
+                aper += 1
+                y = np.polyval(coeff, aper)
+                if 0 < y < h-1:
+                    post_aper_lst.append(aper)
+                    post_apercen_lst.append(y)
+                else:
+                    break
+            post_aper_lst = np.array(post_aper_lst)
+            post_apercen_lst = np.array(post_apercen_lst)
+     
+            # find pre apertures
+            aper = aper_lst[0]
+            pre_aper_lst, pre_apercen_lst = [], []
+            while(True):
+                aper -= 1
+                y = np.polyval(coeff, aper)
+                if 0 < y < h-1:
+                    pre_aper_lst.append(aper)
+                    pre_apercen_lst.append(y)
+                else:
+                    break
+            pre_aper_lst = np.array(pre_aper_lst)
+            pre_apercen_lst = np.array(pre_apercen_lst)
+
+            # plot aper_lst, pre-aperture list, and post-aperture list
+            if plot_section and x in plot_cols:
+                _color = 'C%d'%ichannel
+                ax2.plot(y_lst, aper_lst, 'o',
+                        color=_color, ms=3, alpha=0.5)
+                ax2.plot(post_apercen_lst, post_aper_lst, '^',
+                        color=_color, ms=3, alpha=0.5)
+                ax2.plot(pre_apercen_lst, pre_aper_lst, 'v',
+                        color=_color, ms=3, alpha=0.5)
+                _newx = np.arange(aper_lst[0], aper_lst[-1], 0.1)
+                ax2.plot(np.polyval(coeff, _newx), _newx, '-',
+                        color=_color, lw=1, alpha=0.5)
+
+            for y in np.concatenate((apercen_lst, post_apercen_lst, pre_apercen_lst)):
+                mask = np.abs(ally - y) > distance
+                intermask *= mask
+                if plot_section and x in plot_cols:
+                    ax1.axvline(x=y, color='C%d'%ichannel, ls='--', lw=0.5, alpha=0.2)
+
+        if plot_section and x in plot_cols:
+            _yplot = np.copy(xsection)
+            _yplot[~intermask] = np.NaN
+            ax1.plot(_yplot, '-', color='C0', lw=0.7, alpha=0.5)
+
+        notnanindex = np.nonzero(intermask)[0]
+        group_lst = np.split(notnanindex, np.where(np.diff(notnanindex)!=1)[0]+1)
+        fitx_lst, fity_lst, fityerr_lst = [], [], []
+        for group in group_lst:
+            ydata = xsection[group]
+            local_min = allmin[group]
+            idx = np.nonzero(local_min)[0]
+            if idx.size == 0:
+                continue
+            if idx.size == 1:
+                meanx = ydata.argmin() + group[0]
+                mean, std = ydata.min(),0
+            else:
+                i1, i2 = idx[0], idx[-1]+1
+                mean, std, m = get_clip_mean(ydata[i1:i2], high=2, low=3, maxiter=10)
+                meanx = np.arange(i1, i2)[m].mean() + group[0]
+                if std/math.sqrt(mean)>2 and (i2-i1)>=5:
+                    # remove the two largest points
+                    m = ydata[i1:i2].argsort().argsort() < i2-i1-2
+                    mean, std, m = get_clip_mean(ydata[i1:i2], mask=m, high=2, low=3, maxiter=10)
+                    meanx = np.arange(i1,i2)[m].mean() + group[0]
+                ii1 = i1 + group[0]
+                ii2 = i2 + group[0]
+
+                if plot_section and x in plot_cols:
+                    ax1.plot(np.arange(ii1, ii2), xsection[ii1:ii2], ls='-',
+                            color='C3', lw=0.8, alpha=0.8)
+                    if m.sum() < m.size:
+                        ax1.plot(np.arange(ii1, ii2)[~m], xsection[ii1:ii2][~m],
+                            'o', color='gray', ms=3, lw=1, alpha=0.5)
+    
+            fitx_lst.append(meanx)
+            fity_lst.append(mean)
+            fityerr_lst.append(std)
+            #print('%4d %4d %10.6f %10.6f %10.6f'%(
+            #    group[0], group[-1], mean, std, std/math.sqrt(abs(mean))))
+    
+        fitx_lst    = np.array(fitx_lst)
+        fity_lst    = np.array(fity_lst)
+        fityerr_lst = np.array(fityerr_lst)
+
+        maxiter = 5
+        mask = fity_lst > 0
+        for ite in range(maxiter):
+            coeff = np.polyfit(fitx_lst[mask]/h, np.log(fity_lst[mask]), deg=yorder)
+            yres = np.log(fity_lst) - np.polyval(coeff, fitx_lst/h)
+            std = yres[mask].std()
+            allflux = np.polyval(coeff, ally/h)
+            allflux = np.exp(allflux)
+            
+            new_mask = (yres < 2.*std)*(yres > -5.*std)
+            if new_mask.sum()==mask.sum():
+                break
+            mask = new_mask
+        grid.append(allflux)
+
+        if plot_section and x in plot_cols:
+            # plot fitx and fity with errorbars
+            ax1.errorbar(fitx_lst[~mask], fity_lst[~mask], yerr=fityerr_lst[~mask],
+                        fmt='o', mfc='w', mec='C2', ms=3, mew=1,
+                        ecolor='C2', elinewidth=0.8, alpha=0.8)
+            ax1.errorbar(fitx_lst[mask], fity_lst[mask], yerr=fityerr_lst[mask],
+                        fmt='o', mfc='C2', mec='C2', ms=3, mew=1,
+                        ecolor='C2', elinewidth=0.8, alpha=0.8)
+            ax1.plot(ally, allflux, '-', color='C2', lw=0.7)
+
+            _ymin, _ymax = fity_lst.min(), fity_lst.max()
+            _y1, _y2 = 1.2*_ymin-0.2*_ymax, 1.2*_ymax-0.2*_ymin
+            ax1.set_ylim(_y1, _y2)
+            ax1.set_xlim(0, h-1)
+            ax1.text(0.03*h, 0.2*_y1+0.8*_y2, 'x=%d'%x, fontsize=10, alpha=0.6)
+            if i < 4:
+                ax1.set_xticklabels([])
+            ax1.set_ylabel('Flux')
+            ax2.set_ylabel('Aperture Number')
+
+    if plot_section:
+        fig1.savefig(fig_section)
+        plt.close(fig1)
+
+    grid = np.array(grid)
+    stray = np.zeros_like(data, dtype=data.dtype)
+
+    for y in np.arange(h):
+        f = intp.InterpolatedUnivariateSpline(cols, grid[:,y], k=3)
+        stray[y,:] = f(np.arange(w))
+
+    return stray
