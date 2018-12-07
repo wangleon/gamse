a6030b34d676ce7b02a66a66445d62eb20bae383
Liang Wang <wang.leon@gmail.com>
2018-12-07 16:23:29 +0100

    adjust the structure of pipelines. move some functions from common to __init__.py

--- edrs/pipelines/__init__.py
+++ edrs/pipelines/__init__.py
@@ -1,3 +1,177 @@
-from .reduction import Reduction
+import os
+import logging
+logger = logging.getLogger(__name__)
+import configparser
+
+import numpy as np
+import astropy.io.fits as fits
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tck
+
+from ..utils.obslog import read_log, find_log
+from ..utils.misc   import write_system_info
 
 from . import common
+from . import xinglong216hrs
+from . import foces
+from . import levy
+from . import sarg
+
+#from .reduction import Reduction
+
+def reduce_echelle():
+    '''Automatically select the instrument and reduce echelle spectra
+    accordingly.
+
+    Available instruments include:
+        
+        * *FOCES*: FOCES on 2m Fraunhofer Telescope in Wendelstein Observatory,
+            Germany.
+        * *Xinglong216HRS*: HRS on 2.16m telescope in Xinglong Station, China.
+
+    '''
+
+    # initialize running log
+    log_fmt = ' '.join(['*',
+                        '%(asctime)s.%(msecs)03d',
+                        '[%(levelname)s]',
+                        '%(name)s - %(lineno)d - %(funcName)s():'+os.linesep,
+                        ' %(message)s'+os.linesep+'-'*80,
+                        ])
+    logging.basicConfig(filename='edrs.log',level=logging.DEBUG,
+            format=log_fmt, datefmt='%Y-%m-%dT%H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    # write system info
+    write_system_info()
+
+    # load config file in current directory
+    config_file_lst = [fname for fname in os.listdir(os.curdir)
+                        if fname[-4:]=='.cfg']
+    config = configparser.ConfigParser(
+                inline_comment_prefixes = (';','#'),
+                interpolation           = configparser.ExtendedInterpolation(),
+                )
+    config.read(config_file_lst)
+
+    # find telescope and instrument from config file
+    section = config['data']
+    telescope  = section['telescope']
+    instrument = section['instrument']
+
+    logger.info('Start reducing %s, %s data'%(telescope, instrument))
+
+    if telescope == 'Fraunhofer' and instrument == 'FOCES':
+        foces.reduce()
+    elif telescope == 'Xinglong216' and instrument == 'HRS':
+        xinglong216hrs.reduce()
+    elif telescope == 'APF' and instrument == 'Levy':
+        levy.reduce()
+    else:
+        print('Unknown Instrument: %s - %s'%(telescope, instrument))
+        exit()
+
+def make_log():
+    '''Scan the path to the raw FITS files and generate an observing log.
+    '''
+    config_file_lst = []
+
+    # find local config file
+    for fname in os.listdir(os.curdir):
+        if fname[-4:]=='.cfg':
+            config_file_lst.append(fname)
+
+    # load both built-in and local config files
+    config = configparser.ConfigParser(
+                inline_comment_prefixes = (';','#'),
+                interpolation           = configparser.ExtendedInterpolation(),
+                )
+    config.read(config_file_lst)
+
+    section = config['data']
+    telescope  = section['telescope']
+    instrument = section['instrument']
+    rawdata    = section['rawdata']
+
+    if telescope == 'Fraunhofer' and instrument == 'FOCES':
+        foces.make_log(rawdata)
+    elif telescope == 'Xinglong216' and instrument == 'HRS':
+        xinglong216hrs.make_log(rawdata)
+    elif telescope == 'APF' and instrument == 'Levy':
+        levy.make_log(rawdata)
+    else:
+        print('Unknown Instrument: %s - %s'%(telescope, instrument))
+        exit()
+
+def show_spectra1d(filename_lst):
+    '''Show 1-D spectra in a pop-up window.
+
+    Args:
+        filename_lst (list): List of filenames of 1-D spectra.
+    '''
+    spec_lst = []
+    for filename in filename_lst:
+        data = fits.getdata(filename)
+        spec = {}
+        for row in data:
+            order = row['order']
+            wave  = row['wavelength']
+            flux  = row['flux']
+            spec[order] = (wave, flux)
+        spec_lst.append(spec)
+
+    fig = plt.figure(figsize=(15, 8), dpi=150)
+    ax = fig.add_axes([0.07, 0.1, 0.88, 0.8])
+
+    def plot_order(order):
+        ax.cla()
+        ax.currentorder = order
+        wave_min, wave_max = 1e9, 0
+        flux_min = 1e9
+        for i, spec in enumerate(spec_lst):
+            if order in spec:
+                wave = spec[order][0]
+                flux = spec[order][1]
+                ax.plot(wave, flux, '-', alpha=0.8, lw=0.8,
+                        label=os.path.basename(filename_lst[i]))
+                wave_min = min(wave_min, wave.min())
+                wave_max = max(wave_max, wave.max())
+                flux_min = min(flux_min, flux.min())
+        leg = ax.legend(loc='upper right')
+        leg.get_frame().set_alpha(0.1)
+        ax.set_xlabel(u'Wavelength (\xc5)', fontsize=12)
+        ax.set_ylabel('Flux', fontsize=12)
+        ax.set_title('Order %d'%(order), fontsize=14)
+        ax.set_xlim(wave_min, wave_max)
+        ax.axhline(y=0, color='k', ls='--', lw=0.5)
+        if flux_min > 0:
+            ax.set_ylim(0,)
+        ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g'))
+        ax.yaxis.set_major_formatter(tck.FormatStrFormatter('%g'))
+        fig.canvas.draw()
+
+    def on_key(event):
+        if event.key == 'up':
+            can_plot = False
+            for spec in spec_lst:
+                if ax.currentorder + 1 in spec:
+                    can_plot=True
+                    break
+            if can_plot:
+                plot_order(ax.currentorder + 1)
+        elif event.key == 'down':
+            can_plot = False
+            for spec in spec_lst:
+                if ax.currentorder - 1 in spec:
+                    can_plot=True
+                    break
+            if can_plot:
+                plot_order(ax.currentorder - 1)
+        else:
+            pass
+
+    order0 = list(spec_lst[0].keys())[0]
+    plot_order(order0)
+
+    fig.canvas.mpl_connect('key_press_event', on_key)
+    plt.show()
--- edrs/pipelines/common.py
+++ edrs/pipelines/common.py
@@ -1,136 +1,6 @@
-import os
-import logging
-logger = logging.getLogger(__name__)
-import configparser
-
 import numpy as np
-import astropy.io.fits as fits
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tck
-
-from ..utils.obslog import read_log, find_log
-from ..utils.misc   import write_system_info
-
-from . import xinglong216hrs
-from . import foces
-from . import levy
-from . import sarg
-
-def reduce_echelle():
-    '''Automatically select the instrument and reduce echelle spectra
-    accordingly.
-
-    Available instruments include:
-        
-        * *FOCES*: FOCES on 2m Fraunhofer Telescope in Wendelstein Observatory,
-            Germany.
-        * *Xinglong216HRS*: HRS on 2.16m telescope in Xinglong Station, China.
-
-    '''
-
-    # initialize running log
-    log_fmt = ' '.join(['*',
-                        '%(asctime)s.%(msecs)03d',
-                        '[%(levelname)s]',
-                        '%(name)s - %(lineno)d - %(funcName)s():'+os.linesep,
-                        ' %(message)s'+os.linesep+'-'*80,
-                        ])
-    logging.basicConfig(filename='edrs.log',level=logging.DEBUG,
-            format=log_fmt, datefmt='%Y-%m-%dT%H:%M:%S')
-    logger = logging.getLogger(__name__)
-
-    # write system info
-    write_system_info()
-
-    # load config file in current directory
-    config_file_lst = [fname for fname in os.listdir(os.curdir)
-                        if fname[-4:]=='.cfg']
-    config = configparser.ConfigParser(
-                inline_comment_prefixes = (';','#'),
-                interpolation           = configparser.ExtendedInterpolation(),
-                )
-    config.read(config_file_lst)
-
-    # find telescope and instrument from config file
-    section = config['data']
-    telescope  = section['telescope']
-    instrument = section['instrument']
-
-    logger.info('Start reducing %s, %s data'%(telescope, instrument))
-
-    if telescope == 'Fraunhofer' and instrument == 'FOCES':
-        foces.reduce()
-    elif telescope == 'Xinglong216' and instrument == 'HRS':
-        xinglong216hrs.reduce()
-    elif telescope == 'APF' and instrument == 'Levy':
-        levy.reduce()
-    else:
-        print('Unknown Instrument: %s - %s'%(telescope, instrument))
-        exit()
-
-def make_log():
-    '''Scan the path to the raw FITS files and generate an observing log.
-    '''
-    config_file_lst = []
-
-    # find local config file
-    for fname in os.listdir(os.curdir):
-        if fname[-4:]=='.cfg':
-            config_file_lst.append(fname)
-
-    # load both built-in and local config files
-    config = configparser.ConfigParser(
-                inline_comment_prefixes = (';','#'),
-                interpolation           = configparser.ExtendedInterpolation(),
-                )
-    config.read(config_file_lst)
-
-    section = config['data']
-    telescope  = section['telescope']
-    instrument = section['instrument']
-    rawdata    = section['rawdata']
-
-    if telescope == 'Fraunhofer' and instrument == 'FOCES':
-        foces.make_log(rawdata)
-    elif telescope == 'Xinglong216' and instrument == 'HRS':
-        xinglong216hrs.make_log(rawdata)
-    elif telescope == 'APF' and instrument == 'Levy':
-        levy.make_log(rawdata)
-    else:
-        print('Unknown Instrument: %s - %s'%(telescope, instrument))
-        exit()
-
-def get_instrument():
-    '''Find the telescope and instrument by checking the raw FITS files.
-
-    Returns:
-        str: Name of the instrument.
-    '''
-    config_file = find_config('./')
-    config = read_config(config_file)
-    section = config['data']
-    telescope  = section['telescope']
-    instrument = section['instrument']
-    return telescope, instrument
-
-
-def find_rawdata():
-    '''Find the path to the raw images.
-
-    Returns:
-        *str* or *None*: Path to the raw images. Return *None* if path not found.
-    '''
-
-    if os.path.exists('rawdata'):
-        return 'rawdata'
-    else:
-        config_file = find_config('./')
-        config = read_config(config_file)
-        if config.has_section('path') and \
-           config.has_option('path', 'rawdata'):
-            return config.get_option('path', 'rawdata')
-        else:
-            return None
 
 def plot_spectra1d():
     '''Plot 1d spectra.
@@ -198,75 +68,45 @@ def plot_spectra1d():
             fig.savefig(os.path.join(image_path, 'spec_%s_%02d.png'%(item.fileid, int(io/10.))))
             plt.close(fig)
 
-def show_spectra1d(filename_lst):
-    '''Show 1-D spectra in a pop-up window.
+
+def plot_background_aspect1(data, stray, figname):
+    '''Plot a figure showing the image before background correction and the
+    stray light.
 
     Args:
-        filename_lst (list): List of filenames of 1-D spectra.
+        data (:class:`numpy.ndarray`): Image before background correction.
+        stray (:class:`numpy.ndarray`): Stray light.
+        figname (str): Name of the output figure.
+
     '''
-    spec_lst = []
-    for filename in filename_lst:
-        data = fits.getdata(filename)
-        spec = {}
-        for row in data:
-            order = row['order']
-            wave  = row['wavelength']
-            flux  = row['flux']
-            spec[order] = (wave, flux)
-        spec_lst.append(spec)
+    h, w = data.shape
 
-    fig = plt.figure(figsize=(15, 8), dpi=150)
-    ax = fig.add_axes([0.07, 0.1, 0.88, 0.8])
+    fig = plt.figure(figsize=(16,7), dpi=150)
+    _width = 0.37
+    _height = _width/w*h*16/7
 
-    def plot_order(order):
-        ax.cla()
-        ax.currentorder = order
-        wave_min, wave_max = 1e9, 0
-        flux_min = 1e9
-        for i, spec in enumerate(spec_lst):
-            if order in spec:
-                wave = spec[order][0]
-                flux = spec[order][1]
-                ax.plot(wave, flux, '-', alpha=0.8, lw=0.8,
-                        label=os.path.basename(filename_lst[i]))
-                wave_min = min(wave_min, wave.min())
-                wave_max = max(wave_max, wave.max())
-                flux_min = min(flux_min, flux.min())
-        leg = ax.legend(loc='upper right')
-        leg.get_frame().set_alpha(0.1)
-        ax.set_xlabel(u'Wavelength (\xc5)', fontsize=12)
-        ax.set_ylabel('Flux', fontsize=12)
-        ax.set_title('Order %d'%(order), fontsize=14)
-        ax.set_xlim(wave_min, wave_max)
-        ax.axhline(y=0, color='k', ls='--', lw=0.5)
-        if flux_min > 0:
-            ax.set_ylim(0,)
-        ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g'))
-        ax.yaxis.set_major_formatter(tck.FormatStrFormatter('%g'))
-        fig.canvas.draw()
+    ax21 = fig.add_axes([0.06, 0.1, _width, _height])
+    ax22 = fig.add_axes([0.55, 0.1, _width, _height])
+    ax21c = fig.add_axes([0.06+_width+0.01, 0.1, 0.015, _height])
+    ax22c = fig.add_axes([0.55+_width+0.01, 0.1, 0.015, _height])
 
-    def on_key(event):
-        if event.key == 'up':
-            can_plot = False
-            for spec in spec_lst:
-                if ax.currentorder + 1 in spec:
-                    can_plot=True
-                    break
-            if can_plot:
-                plot_order(ax.currentorder + 1)
-        elif event.key == 'down':
-            can_plot = False
-            for spec in spec_lst:
-                if ax.currentorder - 1 in spec:
-                    can_plot=True
-                    break
-            if can_plot:
-                plot_order(ax.currentorder - 1)
-        else:
-            pass
+    # find the minimum and maximum value of plotting
+    s = np.sort(data.flatten())
+    vmin = s[int(0.05*data.size)]
+    vmax = s[int(0.95*data.size)]
 
-    order0 = list(spec_lst[0].keys())[0]
-    plot_order(order0)
-
-    fig.canvas.mpl_connect('key_press_event', on_key)
-    plt.show()
+    cax_data  = ax21.imshow(data, cmap='gray', vmin=vmin, vmax=vmax)
+    cax_stray = ax22.imshow(stray, cmap='viridis')
+    cs = ax22.contour(stray, colors='r', linewidths=0.5)
+    ax22.clabel(cs, inline=1, fontsize=9, use_clabeltext=True)
+    fig.colorbar(cax_data, cax=ax21c)
+    fig.colorbar(cax_stray, cax=ax22c)
+    for ax in [ax21, ax22]:
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.xaxis.set_major_locator(tck.MultipleLocator(500))
+        ax.xaxis.set_minor_locator(tck.MultipleLocator(100))
+        ax.yaxis.set_major_locator(tck.MultipleLocator(500))
+        ax.yaxis.set_minor_locator(tck.MultipleLocator(100))
+    fig.savefig(figname)
+    plt.close(fig)
