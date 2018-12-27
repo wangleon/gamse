cfb08da9235d8142a44bdf11af54529b3d082aa0
Liang Wang <wang.leon@gmail.com>
2018-12-27 21:24:41 +0100

    modify the script of making HIRES logfile

--- docs/source/pipelines/hires/make_hires_log.py
+++ docs/source/pipelines/hires/make_hires_log.py
@@ -3,7 +3,6 @@ import os,sys,re
 import astropy.io.fits as fits
 
 def main():
-    fn_fmt = 'HI\.\d{8}\.\d{5}\.fits'
     head_lst = [
             'fname','datatype','frameno','n_ccds',
             'target','exptime','date','utc',
@@ -16,72 +15,42 @@ def main():
     c = 0
     string_lst = []
     for fname in sorted(os.listdir(sys.argv[1])):
-        if not re.match(fn_fmt,fname):
+        if not re.match('HI\.\d{8}\.\d{5}\.fits', fname):
             continue
         c += 1
-        f = fits.open(os.path.join(sys.argv[1],fname))
-        try:
-            target   = f[0].header['TARGNAME']
-        except:
-            target   = ''
-        if len(f)>1:
-            n_ccds   = len(f)-1
+        filepath = os.path.join(sys.argv[1], fname)
+        hdu_lst = fits.open(filepath)
+        head0 = hdu_lst[0].header
+        hdu_lst.close()
+        target = head0.get('TARGNAME', '')
+        if len(hdu_lst)>1:
+            n_ccds   = len(hdu_lst)-1
         else:
             n_ccds   = 1
-        try:
-            frame = f[0].header['FRAME']
-        except:
-            frame = ''
-        frameno  = f[0].header['FRAMENO']
-        try:
-            equinox  = f[0].header['EQUINOX']
-        except:
-            equinox  = ''
-        try:
-            ra       = f[0].header['RA']
-        except:
-            ra       = ''
-        try:
-            dec      = f[0].header['DEC']
-        except:
-            dec      = ''
-        date     = f[0].header['DATE-OBS']
-        try:
-            utc  = f[0].header['UTC']
-        except:
-            utc  = f[0].header['UT']
-        progid   = f[0].header['PROGID']
-        progpi   = f[0].header['PROGPI']
-        exptime  = f[0].header['ELAPTIME']
-        try:
-            i2in  = f[0].header['IODIN']
-            i2out = f[0].header['IODOUT']
-        except:
-            i2in  = False
-            i2out = True
-        imgtype  = f[0].header['IMAGETYP']
-        try:
-            deckname = f[0].header['DECKNAME']
-        except:
-            deckname = ''
-        try:
-            filter1  = f[0].header['FIL1NAME']
-        except:
-            filter1  = ''
-        try:
-            filter2  = f[0].header['FIL2NAME']
-        except:
-            filter2 = ''
-        snr      = f[0].header['SIG2NOIS']
-        slit_len = f[0].header['SLITLEN']
-        slit_wid = f[0].header['SLITWIDT']
-        resolution = f[0].header['SPECRES']
-        pix_scale  = f[0].header['SPATSCAL']
+        frame      = head0.get('FRAME', '')
+        frameno    = head0.get('FRAMENO')
+        equinox    = head0.get('EQUINOX', '')
+        ra         = head0.get('RA', '')
+        dec        = head0.get('DEC', '')
+        date       = head0.get('DATE-OBS')
+        utc        = head0.get('UTC', head0.get('UT'))
+        progid     = head0.get('PROGID')
+        progpi     = head0.get('PROGPI')
+        exptime    = head0.get('ELAPTIME')
+        i2in       = head0.get('IODIN', False)
+        i2out      = head0.get('IODOUT', True)
+        imgtype    = head0.get('IMAGETYP')
+        deckname   = head0.get('DECKNAME', '')
+        filter1    = head0.get('FIL1NAME', '')
+        filter2    = head0.get('FIL2NAME', '')
+        snr        = head0.get('SIG2NOIS')
+        slit_len   = head0.get('SLITLEN')
+        slit_wid   = head0.get('SLITWIDT')
+        resolution = head0.get('SPECRES')
+        pix_scale  = head0.get('SPATSCAL')
 
-        gain = ','.join(['%4.2f'%f[0].header['CCDGN%02d'%(i+1)] for i in range(n_ccds)])
-        ron = ','.join(['%4.2f'%f[0].header['CCDRN%02d'%(i+1)] for i in range(n_ccds)])
-
-        f.close()
+        gain = ','.join(['%4.2f'%head0['CCDGN%02d'%(i+1)] for i in range(n_ccds)])
+        ron  = ','.join(['%4.2f'%head0['CCDRN%02d'%(i+1)] for i in range(n_ccds)])
 
         # check
         if imgtype.strip() == 'object':
@@ -102,43 +71,20 @@ def main():
         if i2in + i2out != 1:
             print(' warning: %s i2in = %d, i2out = %d'%(fname,i2in,i2out))
         if c != 1 and frameno != prev_frameno + 1:
-            print(' warning: %s frameno (%d) != prev_frameno (%d) + 1'%(fname,frameno,prev_frameno))
+            print(' warning: %s frameno (%d) != prev_frameno (%d) + 1'%(
+                fname,frameno,prev_frameno))
 
-        output = [
-                '%17s'%fname[0:-5],
-                '%3s'%datatype,
-                '%4d'%frameno,
-                '%1d'%n_ccds,
-                '%15s'%target,
-                '%6.1f'%exptime,
-                '%10s'%date,
-                '%11s'%utc,
-                '%1d'%i2in,
-                '%10s'%ra,
-                '%11s'%dec,
-                #'%4s'%frame,
-                #'%6s'%equinox,
-                '%2s'%deckname,
-                '%5s'%filter1,
-                '%5s'%filter2,
-                '%5.1f'%snr,
-                '%14s'%gain,
-                '%14s'%ron,
-                #'%6.3f'%slit_len,
-                '%6s'%str(slit_len),
-                #'%5.3f'%slit_wid,
-                '%5s'%str(slit_wid),
-                #'%6d'%resolution,
-                '%6s'%str(resolution),
-                '%5.3f'%pix_scale,
-                '%6s'%progid,
-                '%s'%progpi,
-                #'%10s'%imgtype,
-                ]
+        output = ['%17s'%fname[0:-5],   '%3s'%datatype,  '%4d'%frameno,
+                  '%1d'%n_ccds,         '%15s'%target,   '%6.1f'%exptime,
+                  '%10s'%date,          '%11s'%utc,      '%1d'%i2in,
+                  '%10s'%ra,            '%11s'%dec,      '%2s'%deckname,
+                  '%5s'%filter1,        '%5s'%filter2,   '%5.1f'%snr,
+                  '%14s'%gain,          '%14s'%ron,      '%6s'%str(slit_len),
+                  '%5s'%str(slit_wid),  '%6s'%str(resolution),
+                  '%5.3f'%pix_scale,    '%6s'%progid,    '%s'%progpi,
+                 ]
         string_lst.append('|'.join(output))
-
         prev_frameno = frameno
-
 
     # save the log file
     log_filename = '%s.log'%date
