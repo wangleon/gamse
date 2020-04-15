920f9612074f699c8c04c95cab634ffa28551ab5
Liang Wang <wang.leon@gmail.com>
2020-04-15 09:33:46 +0800

    XinglongHRS pipeline: remove deprecated class

--- gamse/pipelines/xinglong216hrs/common.py
+++ gamse/pipelines/xinglong216hrs/common.py
@@ -17,7 +17,6 @@ import matplotlib.dates as mdates
 from ...echelle.trace import TraceFigureCommon
 from ...utils.obslog import read_obslog
 from ...utils.onedarray import iterative_savgol_filter
-from ..reduction          import Reduction
 
 def get_region_lst(header, readout_mode):
     """Get a list of (science, overscan) rectangles of the CCD regions based on
@@ -111,10 +110,10 @@ def get_ccd_geometry(header):
         header (:class:`astropy.io.fits.header`): FITS header.
 
     Returns:
-        tuple: A tuple of ``(x1, y1, xbin, ybin, cover, rover)``, where
-        ``(x1, y1)`` is the starting point of science region,
-        ``(xbin, ybin)`` is the CCD binning along X and Y axes,
-        and ``(cover, rover)`` is the numbers of overscan columns and rows.
+        tuple: A tuple of **(x1, y1, xbin, ybin, cover, rover)**, where
+        **(x1, y1)** is the starting point of science region,
+        **(xbin, ybin)** are the CCD binning along X and Y axes,
+        and **(cover, rover)** are the numbers of overscan columns and rows.
 
     See also:
 
@@ -122,6 +121,13 @@ def get_ccd_geometry(header):
         * :func:`get_sci_region`
         * :func:`get_ovr_region`
 
+    Check the information in the FITS header, and determined some important
+    geometric parameters of the image.
+    **(cover, rover)** are the numbers of overscan columns and rows.
+    **rover** is always 0 as the readout direction of this CCD is along X axis
+    and the overscan is after the readout of each row.
+    If the ``ROVER`` in the FITS header is not 0, this function will raise an
+    exception.
     """
     naxis1 = header['NAXIS1']     # size along X axis
     naxis2 = header['NAXIS2']     # size along Y axis
@@ -466,6 +472,7 @@ def correct_overscan(data, header, readout_mode=None):
     Args:
         data (:class:`numpy.ndarray`): Input image data.
         header (:class:`astropy.io.fits.Header`): Input FITS header.
+        readout_mode (str): Readout mode of the CCD.
     
     Returns:
         tuple: A tuple containing:
@@ -628,326 +635,6 @@ class TraceFigure(TraceFigureCommon):
         self.ax2 = self.add_axes([0.52,0.50,0.43,0.40])
         self.ax3 = self.add_axes([0.52,0.10,0.43,0.40])
         self.ax4 = self.ax3.twinx()
-
-class Xinglong216HRS(Reduction):
-
-    def __init__(self):
-        super(Xinglong216HRS, self).__init__(instrument='Xinglong216HRS')
-
-    def overscan(self):
-        '''
-        Overscan correction for Xinglong 2.16m Telescope HRS.
-
-        .. csv-table:: Accepted options in config file
-           :header: Option, Type, Description
-           :widths: 20, 10, 50
-
-           **skip**,    *bool*, Skip this step if *yes* and **mode** = *'debug'*.
-           **suffix**,  *str*,  Suffix of the corrected files.
-           **plot**,    *bool*, Plot the overscan levels if *yes*.
-           **var_fig**, *str*,  Filename of the overscan variation figure.
-
-        '''
-
-
-        def fix_cr(a):
-            m = a.mean(dtype=np.float64)
-            s = a.std(dtype=np.float64)
-            mask = a > m + 3.*s
-            if mask.sum()>0:
-                x = np.arange(a.size)
-                f = InterpolatedUnivariateSpline(x[~mask],a[~mask],k=3)
-                return f(x)
-            else:
-                return a
-        
-        # find output suffix for fits
-        self.output_suffix = self.config.get('overscan', 'suffix')
-
-        if self.config.getboolean('overscan', 'skip'):
-            logger.info('Skip [overscan] according to the config file')
-            self.input_suffix = self.output_suffix
-            return True
-
-        # keywords for mask
-        saturation_adu = 65535
-
-        # path alias
-        rawdata = self.paths['rawdata']
-        midproc = self.paths['midproc']
-        report  = self.paths['report']
-
-        # loop over all files (bias, dark, ThAr, flat...)
-        # to correct for the overscan
-
-        # prepare the item list
-        item_lst = [item for item in self.log]
-
-        for i, item in enumerate(item_lst):
-            logger.info('Correct overscan for item %3d: "%s"'%(
-                         item.frameid, item.fileid))
-
-            # read FITS data
-            filename = '%s%s.fits'%(item.fileid, self.input_suffix)
-            filepath = os.path.join(rawdata, filename)
-            data, head = fits.getdata(filepath, header=True)
-
-            h, w = data.shape
-            x1, x2 = w-head['COVER'], w
-
-            # find the overscan level along the y-axis
-            ovr_lst1 = data[0:h//2,x1+2:x2].mean(dtype=np.float64, axis=1)
-            ovr_lst2 = data[h//2:h,x1+2:x2].mean(dtype=np.float64, axis=1)
-
-            ovr_lst1_fix = fix_cr(ovr_lst1)
-            ovr_lst2_fix = fix_cr(ovr_lst2)
-
-            # apply the sav-gol fitler to the mean of overscan
-            ovrsmooth1 = savgol_filter(ovr_lst1_fix, window_length=301, polyorder=3)
-            ovrsmooth2 = savgol_filter(ovr_lst2_fix, window_length=301, polyorder=3)
-
-            # plot the overscan regions
-            if i%5 == 0:
-                fig = plt.figure(figsize=(10,6), dpi=150)
-
-            ax1 = fig.add_axes([0.08, 0.83-(i%5)*0.185, 0.42, 0.15])
-            ax2 = fig.add_axes([0.55, 0.83-(i%5)*0.185, 0.42, 0.15])
-
-            ax1.plot([0,0],[ovr_lst1_fix.min(), ovr_lst1_fix.max()], 'w-', alpha=0)
-            _y1, _y2 = ax1.get_ylim()
-            ax1.plot(np.arange(0, h//2), ovr_lst1, 'r-', alpha=0.3)
-            ax1.set_ylim(_y1, _y2)
-
-            ax2.plot([0,0],[ovr_lst2_fix.min(), ovr_lst2_fix.max()], 'w-', alpha=0)
-            _y1, _y2 = ax2.get_ylim()
-            ax2.plot(np.arange(h//2, h), ovr_lst2, 'b-', alpha=0.3)
-            ax2.set_ylim(_y1, _y2)
-
-            ax1.plot(np.arange(0, h//2), ovrsmooth1, 'm', ls='-')
-            ax2.plot(np.arange(h//2, h), ovrsmooth2, 'c', ls='-')
-            ax1.set_ylabel('ADU')
-            ax2.set_ylabel('')
-            ax1.set_xlim(0, h//2-1)
-            ax2.set_xlim(h//2, h-1)
-            for ax in [ax1, ax2]:
-                _x1, _x2 = ax.get_xlim()
-                _y1, _y2 = ax.get_ylim()
-                _x = 0.95*_x1 + 0.05*_x2
-                _y = 0.20*_y1 + 0.80*_y2
-                ax.text(_x, _y, item.fileid, fontsize=9)
-                for tick in ax.xaxis.get_major_ticks():
-                    tick.label1.set_fontsize(9)
-                for tick in ax.yaxis.get_major_ticks():
-                    tick.label1.set_fontsize(9)
-                ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g'))
-                ax.yaxis.set_major_formatter(tck.FormatStrFormatter('%g'))
-                ax.xaxis.set_major_locator(tck.MultipleLocator(500))
-                ax.xaxis.set_minor_locator(tck.MultipleLocator(100))
-            if i%5==4 or i==len(item_lst)-1:
-                ax1.set_xlabel('Y (pixel)')
-                ax2.set_xlabel('Y (pixel)')
-                figname = 'overscan_%02d.png'%(i//5+1)
-                figfile = os.path.join(report, figname)
-                fig.savefig(figfile)
-                logger.info('Save image: %s'%figfile)
-                plt.close(fig)
-
-            # determine shape of output image (also the shape of science region)
-            y1 = head['CRVAL2']
-            y2 = y1 + head['NAXIS2'] - head['ROVER']
-            ymid = (y1 + y2)//2
-            x1 = head['CRVAL1']
-            x2 = x1 + head['NAXIS1'] - head['COVER']
-            newshape = (y2-y1, x2-x1)
-
-            # find the saturation mask
-            mask_sat = (data[y1:y2,x1:x2]>=saturation_adu)
-            # get bad pixel mask
-            bins = (head['RBIN'], head['CBIN'])
-            mask_bad = self._get_badpixel_mask(newshape, bins=bins)
-
-            mask = np.int16(mask_sat)*4 + np.int16(mask_bad)*2
-            # save the mask
-            mask_table = array_to_table(mask)
-            maskname = '%s%s.fits'%(item.fileid, self.mask_suffix)
-            maskpath = os.path.join(midproc, maskname)
-            fits.writeto(maskpath, mask_table, overwrite=True)
-
-            # subtract overscan
-            new_data = np.zeros(newshape, dtype=np.float64)
-            ovrdata1 = np.transpose(np.repeat([ovrsmooth1],x2-x1,axis=0))
-            ovrdata2 = np.transpose(np.repeat([ovrsmooth2],x2-x1,axis=0))
-            new_data[y1:ymid, x1:x2] = data[y1:ymid,x1:x2] - ovrdata1
-            new_data[ymid:y2, x1:x2] = data[ymid:y2,x1:x2] - ovrdata2
-
-            # fix bad pixels
-            new_data = fix_pixels(new_data, mask_bad, 'x', 'linear')
-
-            # update fits header
-            head['HIERARCH GAMSE OVERSCAN']        = True
-            head['HIERARCH GAMSE OVERSCAN METHOD'] = 'smooth'
-
-            # save data
-            outname = '%s%s.fits'%(item.fileid, self.output_suffix)
-            outpath = os.path.join(midproc, outname)
-            fits.writeto(outpath, new_data, head, overwrite=True)
-            print('Correct Overscan {} -> {}'.format(filename, outname))
-
-
-        logger.info('Overscan corrected. Change suffix: %s -> %s'%
-                    (self.input_suffix, self.output_suffix))
-        self.input_suffix = self.output_suffix
-
-    def bias(self):
-        '''Bias corrrection for Xinglong 2.16m Telescope HRS.
-
-        .. csv-table:: Accepted options in config file
-           :header: Option, Type, Description
-           :widths: 20, 10, 50
-
-           **skip**,        *bool*,  Skip this step if *yes* and **mode** = *'debug'*.
-           **suffix**,      *str*,   Suffix of the corrected files.
-           **cosmic_clip**, *float*, Upper clipping threshold to remove cosmic-rays.
-           **file**,        *str*,   Name of bias file.
-
-        '''
-        self.output_suffix = self.config.get('bias', 'suffix')
-
-        if self.config.getboolean('bias', 'skip'):
-            logger.info('Skip [bias] according to the config file')
-            self.input_suffix = self.output_suffix
-            return True
-
-        midproc = self.paths['midproc']
-
-        bias_id_lst = self.find_bias()
-
-        if len(bias_id_lst) == 0:
-            # no bias frame found. quit this method.
-            # update suffix
-            logger.info('No bias found.')
-            return True
-
-        infile_lst = [os.path.join(midproc,
-                        '%s%s.fits'%(item.fileid, self.input_suffix))
-                        for item in self.log if item.frameid in bias_id_lst]
-
-        # import and stack all bias files in a data cube
-        tmp = [fits.getdata(filename, header=True) for filename in infile_lst]
-        all_data, all_head = list(zip(*tmp))
-        all_data = np.array(all_data)
-
-        if self.config.has_option('bias', 'cosmic_clip'):
-            # use sigma-clipping method to mask cosmic rays
-            cosmic_clip = self.config.getfloat('bias', 'cosmic_clip')
-
-            all_mask = np.ones_like(all_data, dtype=np.bool)
-
-            mask = (all_data == all_data.max(axis=0))
-        
-            niter = 0
-            maxiter = 2
-            while niter <= maxiter:
-                niter += 1
-                mdata = np.ma.masked_array(all_data, mask=mask)
-                # calculate mean and standard deviation
-                mean = mdata.mean(axis=0, dtype=np.float64).data
-                std  = mdata.std(axis=0, dtype=np.float64, ddof=1).data
-                # initialize new mask
-                new_mask = np.ones_like(mask)>0
-                # masking all the upper outliers.
-                for i in np.arange(all_data.shape[0]):
-                    new_mask[i,:,:] = all_data[i,:,:] > mean + cosmic_clip*std
-                mask = new_mask
-
-            mdata = np.ma.masked_array(all_data, mask=mask)
-            bias = mdata.mean(axis=0, dtype=np.float64).data
-        else:
-            # no sigma clipping
-            bias = alldata.mean(axis=0, dtype=np.float64)
-
-        # create new FITS Header for bias
-        head = fits.Header()
-        head['HIERARCH GAMSE BIAS NFILE'] = len(bias_id_lst)
-
-        # get final bias filename from the config file
-        bias_file = self.config.get('bias', 'file')
-
-        if self.config.has_option('bias', 'smooth_method'):
-            # perform smoothing for bias
-            smooth_method = self.config.get('bias', 'smooth_method')
-            smooth_method = smooth_method.strip().lower()
-
-            logger.info('Smoothing bias: %s'%smooth_method)
-
-            if smooth_method in ['gauss','gaussian']:
-                # perform 2D gaussian smoothing
-
-                smooth_sigma = self.config.getint('bias', 'smooth_sigma')
-                smooth_mode  = self.config.get('bias', 'smooth_mode')
-
-                logger.info('Smoothing bias: sigma = %f'%smooth_sigma)
-                logger.info('Smoothing bias: mode = %s'%smooth_mode)
-
-                h, w = bias.shape
-                bias_smooth = np.zeros((h, w), dtype=np.float64)
-                bias_smooth[0:h/2,:] = gaussian_filter(bias[0:h/2,:],
-                                                       smooth_sigma,
-                                                       mode=smooth_mode)
-                bias_smooth[h/2:h,:] = gaussian_filter(bias[h/2:h,:],
-                                                       smooth_sigma,
-                                                       mode=smooth_mode)
-
-                logger.info('Smoothing bias: Update bias FITS header')
-
-                head['HIERARCH GAMSE BIAS SMOOTH']        = True
-                head['HIERARCH GAMSE BIAS SMOOTH METHOD'] = 'GAUSSIAN'
-                head['HIERARCH GAMSE BIAS SMOOTH SIGMA']  = smooth_sigma
-                head['HIERARCH GAMSE BIAS SMOOTH MODE']   = smooth_mode
-
-            else:
-                pass
-
-            # bias_data is a proxy for bias to be corrected for each frame
-            bias_data = bias_smooth
-
-            # plot comparison between un-smoothed and smoothed data
-            self.plot_bias_smooth(bias, bias_smooth)
-
-        else:
-            # no smoothing
-            logger.info('No smoothing parameter for bias. Skip bias smoothing')
-            head['HIERARCH GAMSE BIAS SMOOTH'] = False
-            bias_data = bias
-
-        # save the bias to FITS
-        fits.writeto(bias_file, bias_data, head, overwrite=True)
-        
-        self.plot_bias_variation(all_data, all_head, time_key='DATE-STA')
-
-        # finally all files are corrected for the bias
-        for item in self.log:
-            if item.frameid not in bias_id_lst:
-                infile  = '%s%s.fits'%(item.fileid, self.input_suffix)
-                outfile = '%s%s.fits'%(item.fileid, self.output_suffix)
-                inpath  = os.path.join(midproc, infile)
-                outpath = os.path.join(midproc, outfile)
-                data, head = fits.getdata(inpath, header=True)
-                data_new = data - bias_data
-                # write information into FITS header
-                head['HIERARCH GAMSE BIAS'] = True
-                # save the bias corrected data
-                fits.writeto(outpath, data_new, head, overwrite=True)
-                info = ['Correct bias for item no. %d.'%item.frameid,
-                        'Save bias corrected file: "%s"'%outpath]
-                logger.info((os.linesep+'  ').join(info))
-                print('Correct bias: {} => {}'.format(infile, outfile))
-
-        # update suffix
-        logger.info('Bias corrected. Change suffix: %s -> %s'%
-                    (self.input_suffix, self.output_suffix))
-        self.input_suffix = self.output_suffix
-        return True
 
 def print_wrapper(string, item):
     """A wrapper for log printing for Xinglong216HRS pipeline.
