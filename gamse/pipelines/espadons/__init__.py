06941574600f796124832e84e5194ab9243d9d5d
Liang Wang <wang.leon@gmail.com>
2021-03-05 23:41:41 +0800

    ESPADONS: add espadons module

---
+++ gamse/pipelines/espadons/__init__.py
@@ -0,0 +1,16 @@
+import os
+import re
+import sys
+import datetime
+import dateutil.parser
+import configparser
+
+import numpy as np
+import astropy.io.fits as fits
+from astropy.table import Table
+
+def make_config():
+    pass
+
+def make_obslog():
+    pass
